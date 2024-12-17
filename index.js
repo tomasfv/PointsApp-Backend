55d966aa3c837058e90e43b10e82e396b4b23615
tomasfv <tomas.fernandez@studiobando.com>
2024-12-17 07:36:04 -0300

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const port = process.env.PORT || 3001;
+
 
 // Syncing all the models at once.
 
 // Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log("Listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`Listening at port ${port}`); // eslint-disable-line no-console
   });
 });
